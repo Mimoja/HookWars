79cc2a628a8ebd1e2be3032f1e696c4ce953a26a
Maximilian Schander <schander@embedded.rwth-aachen.de>
2016-01-13 17:11:54 +0100

    Calculate NOrmals mockup code

--- util.cpp
+++ util.cpp
@@ -18,78 +18,92 @@ double lastTime = 0.0f;
 int nbFrames = 0;
 
 void FPS_init(float delay) {
-    lastTime= glfwGetTime();
+    lastTime = glfwGetTime();
     nbFrames = 0;
     del = delay;
 }
-void FPS_count(){
+
+void FPS_count() {
     double currentTime = glfwGetTime();
     nbFrames++;
-    if ( currentTime - lastTime >= del ){
-        float t = (1000.0*del)/double(nbFrames);
-        printf("%f ms/frame, %.1f frames / second\n",t,1000.0f/t);
+    if (currentTime - lastTime >= del) {
+        float t = (1000.0 * del) / double(nbFrames);
+        printf("%f ms/frame, %.1f frames / second\n", t, 1000.0f / t);
         nbFrames = 0;
         lastTime = glfwGetTime(); // TODO can we add the delay and save time?
     }
 }
 
-bool loadModelFromFile( const char * path,
-                        std::vector<unsigned short> & indices,
-                        std::vector<glm::vec3> & vertices,
-                        std::vector<glm::vec2> & uvs,
-                        std::vector<glm::vec3> & normals){
+void calculateNormals( std::vector<glm::vec3> & vertices, 
+                       std::vector<glm::vec3> & normals,
+                       std::vector<unsigned short> & indices,
+                       int faceCount) {
+
+    for (unsigned int i = 0; i < faceCount; i++) {
+        //glm::vec3 Normal = glm::normalize(glm::cross(v1, v2));
+    }
+}
+
+bool loadModelFromFile(const char * path,
+    std::vector<unsigned short> & indices,
+    std::vector<glm::vec3> & vertices,
+    std::vector<glm::vec2> & uvs,
+    std::vector<glm::vec3> & normals) {
 
     Assimp::Importer importer;
     printf("Staring importer\n");
 
     const aiScene* scene = importer.ReadFile(path, 0);
-    if( !scene) {
-        printf("%s\n",importer.GetErrorString());
+    if (!scene) {
+        printf("%s\n", importer.GetErrorString());
         getchar();
         return false;
     }
-    
-    if(scene->mNumMeshes==0){
+
+    if (scene->mNumMeshes == 0) {
         printf("No mesh found!\n");
         return false;
     }
 
     printf("success!!\n");
     const aiMesh* mesh = scene->mMeshes[0];
-        
+
     vertices.reserve(mesh->mNumVertices);
     uvs.reserve(mesh->mNumVertices);
     normals.reserve(mesh->mNumVertices);
-    indices.reserve(3*mesh->mNumFaces);
+    indices.reserve(3 * mesh->mNumFaces);
 
 
-    printf("Reading %d Verticies\n",mesh->mNumVertices);
-    for(unsigned int i=0; i<mesh->mNumVertices; i++){
+    printf("Reading %d Verticies\n", (int) mesh->mNumVertices);
+    for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
         aiVector3D pos = mesh->mVertices[i];
         vertices.push_back(glm::vec3(pos.x, pos.y, pos.z));
     }
-     
-    if( mesh->HasTextureCoords(0)){
-        printf("Reading %d UVs\n",mesh->mNumUVComponents);
-        for(unsigned int i=0; i<mesh->mNumVertices; i++){
+
+    if (mesh->HasTextureCoords(0)) {
+        printf("Reading %d UVs\n", mesh->mNumUVComponents);
+        for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
             aiVector3D UVW = mesh->mTextureCoords[0][i];
             uvs.push_back(glm::vec2(UVW.x, UVW.y));
-        }   
-    }
-    if(mesh->HasNormals()){
-        printf("Reading Normals\n");
-        for(unsigned int i=0; i<mesh->mNumVertices; i++){
-            aiVector3D n = mesh->mNormals[i];
-            normals.push_back(glm::vec3(n.x, n.y, n.z));
         }
     }
-    if(mesh->HasFaces()){
+    if (mesh->HasFaces()) {
         printf("Reading %d Faces as Indices\n", mesh->mNumFaces);
-        for (unsigned int i=0; i<mesh->mNumFaces; i++){
+        for (unsigned int i = 0; i < mesh->mNumFaces; i++) {
             indices.push_back(mesh->mFaces[i].mIndices[0]);
             indices.push_back(mesh->mFaces[i].mIndices[1]);
             indices.push_back(mesh->mFaces[i].mIndices[2]);
         }
     }
+    if (mesh->HasNormals()) {
+        printf("Reading Normals\n");
+        for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
+            aiVector3D n = mesh->mNormals[i];
+            normals.push_back(glm::vec3(n.x, n.y, n.z));
+        }
+    }else{
+        calculateNormals(vertices, normals, indices, mesh->mNumFaces);
+    }
+
     return true;
 }
